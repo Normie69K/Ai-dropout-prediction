960d302869877fe08e8f493f3eac06187eceb3fc
Nomie69K <157805964+Normie69K@users.noreply.github.com>
2025-09-14 19:38:14 +0530

    Implement login screen UI and fragment

---
+++ app/src/main/java/com/sih/alt_f4/fragments/LoginFragment.kt
@@ -0,0 +1,37 @@
+package com.sih.alt_f4.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.sih.alt_f4.R
+import com.sih.alt_f4.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonSignIn.setOnClickListener {
+            // Navigate to the main part of the app
+            findNavController().navigate(R.id.action_loginFragment_to_main_nav_graph)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

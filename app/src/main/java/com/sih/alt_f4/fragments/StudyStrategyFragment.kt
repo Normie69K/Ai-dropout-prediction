94876131587b1155aa8812965beea030670ffca2
Nomie69K <157805964+Normie69K@users.noreply.github.com>
2025-09-14 22:36:08 +0530

    Create study strategy screen

---
+++ app/src/main/java/com/sih/alt_f4/fragments/StudyStrategyFragment.kt
@@ -0,0 +1,37 @@
+package com.sih.alt_f4.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.sih.alt_f4.databinding.FragmentStudyStrategyBinding
+
+class StudyStrategyFragment : Fragment() {
+
+    private var _binding: FragmentStudyStrategyBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentStudyStrategyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set a click listener on the button to navigate back
+        binding.buttonGotIt.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
